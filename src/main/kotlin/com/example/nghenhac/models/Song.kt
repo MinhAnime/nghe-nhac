40c723c52a4a9909ab5a680edc09b57fc1a82522
minh <minhduong019904@gmail.com>
2025-11-16 22:31:57 +0700

    ok

--- src/main/kotlin/com/example/nghenhac/models/Song.kt
+++ src/main/kotlin/com/example/nghenhac/models/Song.kt
@@ -17,22 +17,20 @@ class Song(
     @Column(name = "duration_seconds", nullable = false)
     var durationSeconds: Int,
 
-    // Tên của object file nhạc (.mp3) trên MinIO, phải là duy nhất
     @Column(name = "song_object_name", nullable = false, unique = true)
     val songObjectName: String,
 
-    // Tên của object ảnh bìa (cover) trên MinIO
+
     @Column(name = "cover_art_object_name")
     val coverArtObjectName: String? = null,
 
-    // Mối quan hệ: Nhiều Song thuộc về một Artist
-    @ManyToOne(fetch = FetchType.LAZY) // Chỉ tải Artist khi thực sự cần
-    @JoinColumn(name = "artist_id", nullable = false) // Tên cột khóa ngoại
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artist_id", nullable = false)
     val artist: Artist,
 
-    // Mối quan hệ: Nhiều Song có thể nằm trong nhiều Playlist
+
     @ManyToMany(
-        mappedBy = "songs", // "songs" là tên field trong class Playlist
+        mappedBy = "songs",
         fetch = FetchType.LAZY
     )
     val playlists: MutableSet<Playlist> = mutableSetOf()
