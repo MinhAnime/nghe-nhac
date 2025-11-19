cf69f9b5239c7d96d4d792bb14442e6ac9eeed71
minh <minhduong019904@gmail.com>
2025-11-19 22:25:17 +0700

    ok

--- src/main/kotlin/com/example/nghenhac/controller/SongController.kt
+++ src/main/kotlin/com/example/nghenhac/controller/SongController.kt
@@ -67,6 +67,11 @@ class SongController(
             .build()
     }
 
+    @GetMapping("/search")
+    fun searchSongs(@RequestParam("q") query: String): ResponseEntity<List<SongResponseDTO>> {
+        return ResponseEntity.ok(songService.searchSongs(query))
+    }
+
 
     @ExceptionHandler(EntityNotFoundException::class)
     fun handleNotFound(ex: EntityNotFoundException): ResponseEntity<Map<String, String>> {
