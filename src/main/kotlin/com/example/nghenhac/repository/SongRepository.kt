74902a7245bbc202c798f8b16fb6e3733501222c
minh <minhduong019904@gmail.com>
2025-11-19 16:21:55 +0700

    ok

--- src/main/kotlin/com/example/nghenhac/repository/SongRepository.kt
+++ src/main/kotlin/com/example/nghenhac/repository/SongRepository.kt
@@ -2,8 +2,11 @@ package com.example.nghenhac.repository
 
 import com.example.nghenhac.models.Playlist
 import com.example.nghenhac.models.Song
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -15,5 +18,8 @@ interface SongRepository :JpaRepository<Song, Long> {
     fun findFirstByPlaylistsContains(playlist: Playlist): Song?
 
     @Query("SELECT s FROM Song s JOIN FETCH s.artist")
-    fun findAllWithArtist(): List<Song>
+    fun findAllWithArtist(pageable: Pageable): Page<Song>
+
+    @Query("SELECT s FROM Song s JOIN s.playlists p WHERE p.id = :playlistId")
+    fun findByPlaylistId(@Param("playlistId") playlistId: Long, pageable: Pageable): Page<Song>
 }
